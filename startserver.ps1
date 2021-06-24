c6d89ca46170197a9c3c66cc79a0087eae9bcd82
Lloyd <lloyd@matrix.ca>
2021-06-24 13:23:17 -0400

    add initialiasation for the database, set gitignore to exclude configuration file from repository. change web route to addchild and updatelabel

--- startserver.ps1
+++ startserver.ps1
@@ -1,12 +1,16 @@
 Import-Module Pode
 Start-PodeServer {
     add-type -path mysql/MySql.Data.dll
-    $SQLConnection = New-Object MySql.Data.MySqlClient.MySqlConnection "Server=localhost;uid=pode;pwd=')9ij{pok';"
+    $config = Import-PowerShellDataFile -Path $configFile
+    $SQLConnection = New-Object MySql.Data.MySqlClient.MySqlConnection $config.Connection
     $SQLConnection.open()
     $SQLCommand = New-Object MySql.Data.MySqlClient.MySqlCommand
     $SQLCommand.connection = $SQLConnection
     $SQLDataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter
     $SQLDataAdapter.SelectCommand=$SQLCommand 
+
+    $SQLDataAdapter.SelectCommand.CommandText = "USE heirarchy;";
+    $SQLDataAdapter.SelectCommand.ExecuteNonQuery();
 
     $ip = ip -j a | ConvertFrom-Json | Where-Object ifname -eq eth0 | Select-Object -ExpandProperty addr_info | Select-Object -ExpandProperty local -First 1
     #Attach port 8000 to the local machine address and use HTTP protocol
@@ -17,8 +21,8 @@ Start-PodeServer {
         Write-Host "hierarchy"
     }
 
-    # Set Parent 
-    Add-PodeRoute -Method Post -Path '/parent' -ScriptBlock {
+    # Set AddChild 
+    Add-PodeRoute -Method Post -Path '/AddChild' -ScriptBlock {
         Write-Host $WebEvent.Data.test
         try{
 
@@ -28,8 +32,8 @@ Start-PodeServer {
         Write-Host $SQLDataAdapter.SelectCommand.CommandText
         $SQLDataAdapter.fill($SQLDataSet)
         $row = $SQLDataSet.Tables[0].NewRow()
-        $row.user = $WebEvent.Data.test
-        $row.parent = 0
+        $row.label = $WebEvent.Data.label
+        $row.parent = $WebEvent.Data.parent
         }catch{
             Write-Host $_.Exception.Message
         }
@@ -37,8 +41,9 @@ Start-PodeServer {
     }
 
     # Set child
-    Add-PodeRoute -Method Post -Path '/child' -ScriptBlock {
-        Write-Host $WebEvent.Data.test
+    Add-PodeRoute -Method Post -Path '/UpdateLabel' -ScriptBlock {
+        Write-Host $WebEvent.Data.key
+        Write-Host $WebEvent.Data.label
         Move-PodeResponseUrl -Url '/html'
     }
 
